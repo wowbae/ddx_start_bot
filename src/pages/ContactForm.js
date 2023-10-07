320e4f44de55bf82ee300ea3445e61bb58d7b699
wowbae <kogtev.ruslan@gmail.com>
2023-10-07 19:51:49 +0300

    fix

--- src/pages/ContactForm.js
+++ src/pages/ContactForm.js
@@ -77,7 +77,7 @@ export default function ContactForm (props) {
 
       <div
         className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
-        aria-hidden="true">
+        aria-hidden={true}>
         <div
           className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#f4c175] to-[#9089fc] opacity-70 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
           style={{
@@ -144,15 +144,15 @@ export default function ContactForm (props) {
             </label>
             <div className="relative mt-2.5">
               <div 
-              hidden='true'
+              hidden={true}
               className="absolute inset-y-0 left-0 flex items-center">
                 <label 
-                hidden='true'
+                hidden={true}
                 htmlFor="country" className="sr-only">
                   Country
                 </label>
                 <select
-                  hidden='true'
+                  hidden={true}
                   id="country"
                   name="country"
                   className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
@@ -162,9 +162,9 @@ export default function ContactForm (props) {
                   {/* <option>EU</option> */}
                 </select>
                 <ChevronDownIcon
-                  hidden='true'
+                  hidden={true}
                   className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
-                  aria-hidden="true"
+                  aria-hidden={true}
                 />
               </div>
               <input
@@ -194,13 +194,11 @@ export default function ContactForm (props) {
           </div>
 
           <div>
-            <label htmlFor="gymName" className="
-              block text-sm font-semibold leading-6 text-gray-900">
-              Ваш клуб
-            </label>
-            <div className="mt-2.5">
+            
+            <div 
+            hidden={true}
+            className="mt-2.5">
               <input
-                // defaultValue = {GymNameData ? GymNameData : ''}
                 type="text"
                 defaultValue={gymName || ''}
                 id="gymName"
@@ -212,75 +210,77 @@ export default function ContactForm (props) {
                 sm:text-sm sm:leading-6"
               />
             </div>
-          </div>
-          <div >
-            <label className="block text-sm font-semibold leading-6 text-gray-900">
-              Поменять клуб? 
+            <label className="
+              block text-sm font-semibold leading-6 text-gray-900">
+              Ваш клуб: {gymName}
             </label>
-            <div className="mt-2.5">
-              {/* <MyCombobox gym={props.gym}/> */}
-              <button
-                type="button"
-                onClick={routeChange}
-                className="block w-full rounded-md h-10 border-orange-300 border-2  
-                text-center text-sm font-semibold
-                text-orange-400 shadow-sm hover:bg-orange-300 hover:text-white
-                focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 
-                focus-visible:outline-indigo-600">
-                Да, у меня другой
-              </button>
-            </div>
-          </div>
-        </div>
 
+            <div className='mt-5 inline-flex items-center'> 
+            <Switch.Group as="div" className="
+              flex sm:col-span-2">
 
-        <div className="text-center mt-10">
-
-        <div className='mx-16 '>
-            <Switch.Group as="div" className="text-center flex gap-x-4 sm:col-span-2">
-              <div className="flex h-6 items-center">
+              <div className="
+              flex h-6 items-center"> 
                 <Switch
                   id='switch'
                   checked={agreed}
                   onChange={setAgreed}
                   className={classNames(
-                    agreed ? 'bg-indigo-600' : 'bg-gray-200',
-                    'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                    agreed 
+                    ? 'bg-indigo-600' 
+                    : 'bg-gray-200',
+                    'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2  focus-visible:outline-indigo-600'
                   )}
                 >
-                  <span className="sr-only">Agree to policies</span>
                   <span
-                    aria-hidden="true"
                     className={classNames(
                       agreed ? 'translate-x-3.5' : 'translate-x-0',
                       'h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out'
                     )}
                   />
                 </Switch>
-              </div>
-              <Switch.Label className="text-sm leading-6 text-gray-600">
-                Подтверждаю согласие с {' '}
-                <a href="https://google.ru" className="font-semibold text-indigo-600">
-                  политикой&nbsp;конфиденциальности.
-                </a>
-              </Switch.Label>
-            </Switch.Group>
+                </div>
+                <Switch.Label className="
+                text-start ml-4 text-sm w-auto text-gray-600">
+                  Подтверждаю согласие с {' '}
+                  <a href="https://google.ru" className="font-semibold text-indigo-600">
+                    политикой&nbsp;конфиденциальности.
+                  </a>
+                </Switch.Label>
+              </Switch.Group>
+            </div>
           </div>
+        </div>
 
-          <button
-            type="submit"
-            // onClick={}
-            className="mt-4 w-1/2 rounded-md
-            bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold 
-            text-white shadow-sm hover:bg-indigo-500 focus-visible:outline 
-            focus-visible:outline-2 focus-visible:outline-offset-2 
-            focus-visible:outline-indigo-600"
-            onClick={ handleSubmit }
-          >
-            Отправить
-          </button>
           
-          
+        {/* нижний div это кнопки */}
+        <div className="mt-3">
+          <div className='
+           gap-4 grid grid-cols-2 items-baseline text-center'>
+            <button
+                type="button"
+                onClick={routeChange}
+                className="block w-full rounded-md h-9 border-orange-300 border-2  
+                text-center text-sm font-semibold
+                text-orange-400 shadow-sm hover:bg-orange-300 hover:text-white
+                focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 
+                focus-visible:outline-indigo-600">
+                Сменить клуб
+            </button>
+
+            <button
+              type="submit"
+              // onClick={}
+              className="mt-4 h-9 w-auto rounded-md
+              bg-indigo-600 px-3.5 text-center text-sm font-semibold 
+              text-white shadow-sm hover:bg-indigo-500 focus-visible:outline 
+              focus-visible:outline-2 focus-visible:outline-offset-2 
+              focus-visible:outline-indigo-600"
+              onClick={ handleSubmit }
+            >
+              Отправить
+            </button>
+          </div>       
         </div>
       </form>
     </div>
