af240302e8080159b95170b02eba08e48c9c1b6e
wowbae <kogtev.ruslan@gmail.com>
2023-10-08 18:38:52 +0300

    use effect NO in condition situations!

--- src/components/ui/MyCombobox.js
+++ src/components/ui/MyCombobox.js
@@ -52,7 +52,10 @@ export default function MyCombobox () {
     // <div className="fixed top-16 w-72">
     <div className="">
       <Combobox value={selected || ''} onChange={setSelected}>
-        { selected && routeChange('/form', {state: selected }) }
+        { useEffect(()=>{
+            selected && routeChange('/form', {state: selected })
+          }, [])
+        }
 
         <div className="relative mt-1">
           <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
