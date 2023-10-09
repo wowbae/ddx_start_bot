c1f8dbbcfd058077722a884cc018f3f55e458aeb
wowbae <kogtev.ruslan@gmail.com>
2023-10-10 00:20:03 +0300

    icon

--- src/components/ui/Notification.js
+++ src/components/ui/Notification.js
@@ -3,6 +3,11 @@ import { Fragment, useState } from 'react'
 import { Transition } from '@headlessui/react'
 import { CheckCircleIcon } from '@heroicons/react/outline'
 import { XIcon } from '@heroicons/react/solid'
+import { BanIcon } from "@heroicons/react/outline";
+
+
+
+
 
 export default function Notification (props) {
   const [show, setShow] = useState(true)
@@ -36,7 +41,8 @@ export default function Notification (props) {
               <div className="p-4">
                 <div className="flex items-start">
                   <div className="flex-shrink-0">
-                    <CheckCircleIcon className="h-6 w-6 text-green-400" aria-hidden="true" />
+                    {props.msg[0]!== 'Ошибка' && <CheckCircleIcon className="h-6 w-6 text-green-400" aria-hidden="true" />}
+                    {props.msg[0] === 'Ошибка' && <BanIcon class="h-6 w-6 text-red-500" aria-hidden="true" />}
                   </div>
                   <div className="ml-3 w-0 flex-1 pt-0.5">
                     <p className="text-sm font-medium text-gray-900">{props.msg[0]}</p>
