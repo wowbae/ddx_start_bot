e800f25cb050b3f1ed799b91014fa987968c65c7
wowbae <kogtev.ruslan@gmail.com>
2023-10-12 12:13:01 +0300

    onscroll

--- src/components/ui/MyCombobox.js
+++ src/components/ui/MyCombobox.js
@@ -149,8 +149,12 @@ export default function MyCombobox () {
           > 
             <div ref={resultsContainerRef} onScroll={handleScroll}>
               <Combobox.Options 
-              // onTouchStart={hideKeyboardOnScroll}
-              className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              className="absolute mt-1 max-h-60 w-full overflow-auto 
+              rounded-md bg-white py-1 text-base shadow-lg 
+              ring-1 ring-black ring-opacity-5 
+              focus:outline-none sm:text-sm"
+              onScroll={handleScroll}
+              >
                 {filteredPeople.length === 0 && query !== '' ? (
                   <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
                     Nothing found.
