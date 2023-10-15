1b0c3be95b9cc93d425f5708255eec90487bb35c
wowbae <kogtev.ruslan@gmail.com>
2023-10-15 21:25:00 +0300

    ccc

--- src/components/ui/MyCombobox.js
+++ src/components/ui/MyCombobox.js
@@ -90,7 +90,7 @@ export default function MyCombobox () {
 
         <div className="relative mt-1">
           <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-            <input
+            <Combobox.Input
               type='text'
               id='welcome-input'
               readonly
